2297744fdb730f56817864eda736ae29bbfad134
Juan Tovar <jujotomo@gmail.com>
2020-04-10 22:49:38 -0700

    Lista la interfaz básica

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 #-*- coding: utf-8 -*-
 
 def run():
-    pass
+    while True:
+        command = str(input('''
+            ¿Qué deseas hacer?
+
+            [m]ontar licitación
+            [l]icitar
+            [s]alir
+        ''')).lower()
+
+        if command == 'm':
+            print('Voy a montar licitación')
+
+        elif command == 'l':
+            print('voy a licitar')
+
+        elif command == 's':
+            break
+        else:
+            print('El comando seleccionado no existe')
 
 if __name__ == "__main__":
     run()
